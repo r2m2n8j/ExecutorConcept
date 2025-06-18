609a875175b514f266689feb58912fe62187db3d
Ramanuj <anuj.mca.nitrr.2019@gmail.com>
2025-06-18 11:53:37 +0530

    First commit

---
+++ src/main/java/com/org/executor/framework/executor/ExecutorThread.java
@@ -0,0 +1,7 @@
+package com.org.executor.framework.executor;
+
+public class ExecutorThread {
+    public static void main(String[] args) {
+        System.out.println("Concept of Executor Thread");
+    }
+}
---
+++ src/main/java/com/org/executor/framework/executor/SimpleThread.java
@@ -0,0 +1,7 @@
+package com.org.executor.framework.executor;
+
+public class SimpleThread {
+    public static void main(String[] args) {
+        System.out.println("Simple thread");
+    }
+}
