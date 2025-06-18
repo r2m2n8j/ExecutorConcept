044ff73cebfd2e8e51f514be8672eda1b3040734
Ramanuj <anuj.mca.nitrr.2019@gmail.com>
2025-06-18 19:57:07 +0530

    Executors

--- src/main/java/com/org/executor/framework/executor/ExecutorThread.java
+++ src/main/java/com/org/executor/framework/executor/ExecutorThread.java
@@ -1,7 +1,65 @@
 package com.org.executor.framework.executor;
 
+import java.util.concurrent.*;
+
 public class ExecutorThread {
     public static void main(String[] args) {
+       /*
+        In the SimpleThread class we are creating thread and doing our work(find factorial)
+        But we don't want to do this.
+            We want Java will create thread and we only focus our own work
+                like here finding factorial of all the numbers
+            To OVER COME THIS PROBLEM JAVA INTRODUCE EXECUTOR IN Java 5 version
+        */
         System.out.println("Concept of Executor Thread");
+
+        createMultipleThreadAndCallFactorial();
+
+    }
+    public static void createMultipleThreadAndCallFactorial() {
+        long startTime = System.currentTimeMillis();
+//        Executor executor = Executors.newSingleThreadExecutor();
+        ExecutorService executorService = Executors.newFixedThreadPool(9);
+        Future<?> future = null;
+        for (int i = 1; i <= 10; i++) {
+            int finalI = i;
+            // creating thread one by one and adding it into thread array
+            future = executorService.submit(
+                    () -> {
+                        System.out.println(finalI + " = " + factorial(finalI));
+                    }
+            );
+        }
+        try {
+            future.get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        executorService.shutdown();
+
+
+//        try {
+//            executorService.awaitTermination(4, TimeUnit.SECONDS);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total Time : "+ (endTime-startTime));
+    }
+    // 5 = 5*4*3*2*1
+    public static long factorial(int number) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        int ans = 1;
+        for (int i = number; i > 0; i--) {
+            ans *= i;
+        }
+        return ans;
     }
 }
--- src/main/java/com/org/executor/framework/executor/SimpleThread.java
+++ src/main/java/com/org/executor/framework/executor/SimpleThread.java
@@ -1,7 +1,64 @@
 package com.org.executor.framework.executor;
 
+import javax.swing.plaf.TableHeaderUI;
+
 public class SimpleThread {
     public static void main(String[] args) {
         System.out.println("Simple thread");
+        long currentTime = System.currentTimeMillis();
+        //simpleFactorialCall();
+        createMultipleThreadAndCallFactorial();
+        long finalTime = System.currentTimeMillis();
+        System.out.println("Total Time :: " + (finalTime - currentTime));
+    }
+
+    // Creating Multiple of threads for creating factorial of 1 to 10 numbers
+    public static void createMultipleThreadAndCallFactorial() {
+        // Here I'm creating an array of Thread because I want to wait until all the task not completed.
+
+        Thread[] threads = new Thread[10];
+        for (int i = 1; i <= 10; i++) {
+            int finalI = i;
+            // creating thread one by one and adding it into thread array
+            threads[i - 1] = new Thread(
+                    () -> {
+                        System.out.println(finalI + " = " + factorial(finalI));
+                    }
+            );
+            threads[i - 1].start();
+        }
+        // Here we are waiting until all threading is complete, after that we are executing in next line.
+        // And now we are getting accurate time for completing factorial
+        // Total Time :: 1038
+        // Earlier we are getting  Total Time :: 4
+        // Because main thread was not waiting for the above thread.
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // Simple calling factorial method and create factorial of 1 to 10 numbers
+    public static void simpleFactorialCall() {
+        for (int i = 1; i < 10; i++) {
+            System.out.println(factorial(i));
+        }
+    }
+
+    // 5 = 5*4*3*2*1
+    public static long factorial(int number) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        int ans = 1;
+        for (int i = number; i > 0; i--) {
+            ans *= i;
+        }
+        return ans;
     }
 }
