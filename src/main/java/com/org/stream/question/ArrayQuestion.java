9a85418982b320b2bfd6dd8c5131fbb616eb9bc9
Ramanuj <anuj.mca.nitrr.2019@gmail.com>
2025-08-13 14:07:48 +0530

    use some more method

--- src/main/java/com/org/stream/question/ArrayQuestion.java
+++ src/main/java/com/org/stream/question/ArrayQuestion.java
@@ -1,31 +1,120 @@
 package com.org.stream.question;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ArrayQuestion {
     public static void main(String[] args) {
 
-        divideArrayWithEvenAndOddElement();
+        // divideArrayWithEvenAndOddElement();
+        // arrangeInGroup();
+        // arrangeInGroupUsingStream();
+        containsOnlyInteger();
+
     }
-    // Given a list of integers, divide it into two arrays having an even number and other have odd number
-    public static void divideArrayWithEvenAndOddElement(){
+
+    //Given a list of integer find the product of first two integer
+    public static void productOfFirstTwoInteger(){
+        int [] arr = {11,22,3,4,5};
+        IntStream list = Arrays.stream(arr).limit(2);
+        
+    }
+    // Given a list of strings, ceate a list that contains only integer
+    public static void containsOnlyInteger() {
+        String[] s = { "abc", "123", "23", "axv", "433", "abx" };
+        List<Integer> list = Arrays.stream(s)
+                .filter(str -> str.matches("[0-9]+"))
+                .map(Integer::valueOf)
+                .toList();
+        System.out.println("list of integer "+ list);//list of integer [123, 23, 433]
+    }
+
+    // Given an array of integers, group the number by range in which they belong
+    /*
+     * I/P : int []arr = {2,3,10,14,20,22}
+     * O/P : {0=[2,3], 10=[10,14], 20=[20,22]}
+     * 0 - All numbers thats are between 0 To 9 comes in this group
+     * 10 - All numbers that's are b/w 10 To 19 comes in this group
+     */
+
+    public static void arrangeInGroupUsingStream() {
+        int[] arr = { 2, 2, 14, 3, 4, 14, 17, 10, 20, 22, 32, 11, 25 };
+
+        Map<Integer, List<Integer>> map = Arrays.stream(arr).boxed()
+                .collect(Collectors.groupingBy(
+                        val -> (val / 10) * 10,
+                        Collectors.toList()));
+        System.out.println("Map :: " + map);// Map :: {0=[2, 2, 3, 4], 20=[20, 22, 25], 10=[14, 14, 17, 10, 11], 30=[32]}
+
+        HashMap<Integer, List<Integer>> hashMap = Arrays.stream(arr).boxed()
+                .collect(Collectors.groupingBy(
+                        val -> (val / 10) * 10,
+                        HashMap::new,
+                        Collectors.toList()));
+        System.out.println("hash map = " + hashMap); // hash map = {0=[2, 2, 3, 4], 20=[20, 22, 25], 10=[14, 14, 17, 10, 11], 30=[32]}
+
+        TreeMap<Integer, List<Integer>> treeMap = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        val -> (val / 10) * 10,
+                        TreeMap::new,
+                        Collectors.toList()));
+
+        System.out.println("tree map = " + treeMap); // tree map = {0=[2, 2, 3, 4], 10=[14, 14, 17, 10, 11], 20=[20, 22, 25], 30=[32]}
+
+        LinkedHashMap<Integer, Set<Integer>> linkedHashMap = Arrays.stream(arr).boxed()
+                .collect(Collectors.groupingBy(
+                        val -> (val / 10) * 10,
+                        LinkedHashMap::new,
+                        Collectors.toSet()));
+        System.out.println("linked hash map = " + linkedHashMap);// linked hash map = {0=[2, 3, 4], 10=[17, 10, 11, 14], 20=[20, 22, 25], 30=[32]}
+    }
+
+    public static void arrangeInGroup() {
+        int[] arr = { 2, 3, 4, 14, 17, 10, 20, 22, 32 };
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int val : arr) {
+            int key = (val / 10) * 10;
+            // if (map.containsKey(key)) {
+            // List<Integer> list = map.get(key);
+            // list.add(val);
+            // map.put(key, list);
+            // } else {
+            // List<Integer> list = new ArrayList<>();
+            // list.add(val);
+            // map.put(key, list);
+            // }
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(val);
+        }
+        System.out.println(map);
+    }
+
+    // Given a list of integers, divide it into two arrays having an even number and
+    // other have odd number
+    public static void divideArrayWithEvenAndOddElement() {
         // {1,2,3,4,5,6,7,8,9,10}
-        int []arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         // We can convert it into List<Integer>
         List<Integer> list = Arrays.stream(arr)
                 .boxed()
                 .toList();
 
-        //Using a way to convert arrays into 2 part even and odd
-        //So I'm converting it into Map key = true for even and false for odd
+        // Using a way to convert arrays into 2 part even and odd
+        // So I'm converting it into Map key = true for even and false for odd
 
-        Map<Boolean, List<Integer>> mapList =  list.stream()
-                .collect(Collectors.groupingBy(x->x%2==0,Collectors.toList()));
-                    // We can use partitioningBy as well
-        Map<Boolean, List<Integer>> mapListUsingPartitioningByMethod = list.stream().collect(Collectors.partitioningBy(x->x%2==0, Collectors.toList()));
+        Map<Boolean, List<Integer>> mapList = list.stream()
+                .collect(Collectors.groupingBy(x -> x % 2 == 0, Collectors.toList()));
+        // We can use partitioningBy as well
+        Map<Boolean, List<Integer>> mapListUsingPartitioningByMethod = list.stream()
+                .collect(Collectors.partitioningBy(x -> x % 2 == 0, Collectors.toList()));
         System.out.println(mapList);
         System.out.println(mapListUsingPartitioningByMethod);
         // {false=[1, 3, 5, 7, 9], true=[2, 4, 6, 8, 10]}
@@ -33,7 +122,7 @@ public class ArrayQuestion {
         // Now we need only list not in map
         List<List<Integer>> listOfOddAndEven = mapList.entrySet()
                 .stream()
-                .map(x->x.getValue())
+                .map(x -> x.getValue())
                 .toList();
 
         System.out.println(listOfOddAndEven);
--- src/main/java/com/org/stream/question/StringQuestions.java
+++ src/main/java/com/org/stream/question/StringQuestions.java
@@ -1,60 +1,196 @@
 package com.org.stream.question;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StringQuestions {
     public static void main(String[] args) {
 
-//        occurrenceOfEachWord();
-//        find2ndHighest();
-        findWordsWithRespectToVowel();
+        // occurrenceOfEachWord();
+        // find2ndHighest();
+        // findWordsWithRespectToVowel();
+        // findFirstRepeatedCharacterWithStream();
+        // findFirstNonRepeatedCharacterWithStream();
+        arrangeListOfStringInAscendingOrder();
+        // arrangeListOfStringInAscendingOrderWithAlphabeticalOrder();
+
+    }
+    // List of String arrange the string into ascending order according to it's
+    // length. If two of String has equal length arrange it with their alphabetically
+    // order
+    public static void arrangeListOfStringInAscendingOrderWithAlphabeticalOrder(){
+        List<String> list = Arrays.asList("apple", "a", "kiwi", "fog", "frog","banana", "fig", "pineapple");
+        List<String> sortedString = list.stream()
+                .sorted(
+                    Comparator.comparingInt((String s)-> s.length())
+                    .thenComparing(Comparator.naturalOrder())
+                )
+                .toList();
+                System.out.println(sortedString);
+    }
+
+    public static void arrangeListOfStringInAscendingOrder() {
+        List<String> list = Arrays.asList("apple", "a", "kiwi", "banana", "fig", "pineapple");
+        List<String> sortedString = list.stream()
+                .sorted(Comparator.comparingInt(s -> s.length()))
+                .toList();
+        System.out.println(sortedString);
+    }
+
+    // Given a String, find the first non-repeated character
+    public static void findFirstNonRepeatedCharacterWithStream() {
+        String s = "Hello World He";
+        /** O(n) */
+        Optional<Character> findFirst = s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        ch -> ch, // key = character
+                        LinkedHashMap::new, // preserve order
+                        Collectors.counting() // count occurrences
+                ))
+                .entrySet()
+                .stream()
+                .filter(e -> {
+                    if (e.getKey() != ' ')
+                        return e.getValue() == 1;
+                    return false;
+                }) // keep only repeated ones
+                .map(Map.Entry::getKey) // get the character
+                .findFirst(); // first in original order
+
+        if (findFirst.isPresent())
+            System.out.println("First non repeated char :: " + findFirst.get());
+
+        /** O(n^2) */
+        // Optional<Character> findNonRepeated = s.chars()
+        // .mapToObj(c -> {
+        // return (char)c;
+        // })
+        // .filter(ch -> {
+        // List<Character> list = s.chars()
+        // .mapToObj(c -> (char) c)
+        // .toList();
+        // int freq = 0;
+        // if(ch!=' ')
+        // freq = Collections.frequency(list, ch);
+
+        // return freq==1;
+        // })
+        // .findFirst();
+        // if(findNonRepeated.isPresent()){
+        // System.out.println("First Non Repeated Character :: "+
+        // findNonRepeated.get());
+        // }
+    }
+
+    // Given a String, find the first repeated character
+    public static void findFirstRepeatedCharacterWithStream() {
+        String s = "Hello World";
+        /** O(n) */
+        Optional<Character> findFirst = s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        ch -> ch, // key = character
+                        LinkedHashMap::new, // preserve order
+                        Collectors.counting() // count occurrences
+                ))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() > 1) // keep only repeated ones
+                .map(Map.Entry::getKey) // get the character
+                .findFirst(); // first in original order
+
+        if (findFirst.isPresent())
+            System.out.println("First non repeated char :: " + findFirst.get());
+
+        /** O(n^2) */
+        // Optional<Character> firstRepeated = s.chars() // IntStream of character codes
+        // [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]
+        // .mapToObj(c -> (char) c) // Convert to Character ['H', 'e', 'l', 'l', 'o', '
+        // ', 'W', 'o', 'r', 'l', 'd']
+        // .filter(ch ->{
+        // List<Character> list = s.chars()
+        // .mapToObj(c -> (char)c)
+        // .toList();
+        // // System.out.println(list); ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r',
+        // 'l', 'd']
+        // int freq = Collections.frequency(list, ch);
+        // return freq > 1;
+        // } ) // Keep only repeated characters
+        // .findFirst(); // Get first one
+
+        // if (firstRepeated.isPresent()) {
+        // System.out.println("First Repeated Character :: " + firstRepeated.get());
+        // } else {
+        // System.out.println("All characters are unique");
+        // }
+    }
+
+    public static void findFirstRepeatedCharacter() {
+        String s = "Hello World";
+        int arr[] = new int[256];
+        boolean flag = false;
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i)]++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (arr[s.charAt(i)] > 1) {
+                System.out.println("First Repeated Character :: " + s.charAt(i));
+                flag = true;
+                break;
+            }
+        }
+        if (!flag)
+            System.out.println("All characters are unique");
 
     }
 
     // Given a sentence, find the words with a specified number of vowels
-    public static void findWordsWithRespectToVowel(){
+    public static void findWordsWithRespectToVowel() {
         String s = "Given a sentence, find the words with a specified number of vowels";
         int vowels = 2;
         Arrays.stream(s.split(" "))
-                .filter(x-> x.replaceAll("[^aeiouAEIOU]", "").length() == vowels)
+                .filter(x -> x.replaceAll("[^aeiouAEIOU]", "").length() == vowels)
                 .forEach(System.out::println);
 
     }
 
     // Find the occurrence of each word
-    public static void occurrenceOfEachWord(){
+    public static void occurrenceOfEachWord() {
         String s = "word of each occurrence Find the occurrence of each word";
         Arrays.stream(s.split(" "))
                 .collect(Collectors.groupingBy(
                         word -> word,
-//                        word -> word.length()
-                        Collectors.counting()
-                ))
-                .forEach((word, count)-> System.out.println(word + " " + count));
-//        Map<String, Integer> map = new HashMap<>();
-//        for(String word : s.split(" ")){
-//            map.put(word, map.getOrDefault(word, 0)+1);
-//        }
-//        for(Map.Entry<String, Integer> e: map.entrySet())
-//            System.out.println(e.getKey()+ " "+ e.getValue());
+                        // word -> word.length()
+                        Collectors.counting()))
+                .forEach((word, count) -> System.out.println(word + " " + count));
+        // Map<String, Integer> map = new HashMap<>();
+        // for(String word : s.split(" ")){
+        // map.put(word, map.getOrDefault(word, 0)+1);
+        // }
+        // for(Map.Entry<String, Integer> e: map.entrySet())
+        // System.out.println(e.getKey()+ " "+ e.getValue());
     }
 
     // Find the 2nd highest length word in a sentence
-    public static void find2ndHighest(){
+    public static void find2ndHighest() {
         String s = "Find the 2nd highest length word in a sentence";
 
         String string = ",";
         Arrays.stream(s.split(" "))
-                        .sorted((w1, w2)-> Integer.compare(w2.length(), w1.length()))
-                        .forEach(word -> System.out.print(word + " "));
+                .sorted((w1, w2) -> Integer.compare(w2.length(), w1.length()))
+                .forEach(word -> System.out.print(word + " "));
 
         System.out.println();
         int secondHighest = Arrays.stream(s.split(" "))
-                .map(String :: length)
+                .map(String::length)
                 .sorted(Comparator.reverseOrder())
                 .skip(0)
                 .findFirst()
